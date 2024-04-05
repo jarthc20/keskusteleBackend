ae6e115161ac79c1068876f52b52f3adec7d8e50
jarthc20 <jarthc20@htl-kaindorf.at>
2024-04-05 08:31:32 +0200

    initial push

---
+++ src/main/java/at/htlkaindorf/keskustelebackend/KeskusteleBackendApplication.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.keskustelebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeskusteleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeskusteleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/htlkaindorf/keskustelebackend/KeskusteleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.keskustelebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeskusteleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
