a0e39123c7bdc2e8e7215fe99b0a33d6e02d00d5
jarthc20 <jarthc20@htl-kaindorf.at>
2024-06-07 09:25:50 +0200

    mock data updated

--- src/main/java/at/htlkaindorf/keskustelebackend/KeskusteleBackendApplication.java
+++ src/main/java/at/htlkaindorf/keskustelebackend/KeskusteleBackendApplication.java
@@ -34,9 +34,19 @@ public class KeskusteleBackendApplication {
             Faker faker = Faker.instance();
 
             User admin = new User(null, "admin", "admin@cool.com", "12345tJ!", new ArrayList<>(), Date.valueOf(LocalDate.now()),Date.valueOf(LocalDate.now()));
+            User user = new User(null, "user", "user@cool.com", "12345tJ!", new ArrayList<>(), Date.valueOf(LocalDate.now()),Date.valueOf(LocalDate.now()));
             Chatroom cr = new Chatroom(null, "CR", "", admin, new ArrayList<>());
+            Chatroom cr2 = new Chatroom(null, "CR2", "", user, new ArrayList<>());
             Message message = new Message(null, "Hello World!", OffsetDateTime.now(), admin, null);
+            Message message2 = new Message(null, "Hello cooler World!", OffsetDateTime.now(), user, null);
+            Message message3 = new Message(null, "Thomas is cool", OffsetDateTime.now(), admin, null);
+            Message message4 = new Message(null, "Thomas is cool", OffsetDateTime.now(), admin, null);
+            Message message5 = new Message(null, "Thomas is cool", OffsetDateTime.now(), admin, null);
             cr.addMessage(message);
+            cr.addMessage(message2);
+            cr.addMessage(message3);
+            cr2.addMessage(message4);
+            cr2.addMessage(message5);
 
             chatroomServiceImp.save(cr);
         };
